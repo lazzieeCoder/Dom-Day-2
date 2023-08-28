0d9f1ffa2ef0dfbf95133d62c7ec29f48bbbb682
lazzieeCoder <rupanrrc26@gmail.com>
2023-08-28 12:56:52 +0530

    updated

--- script.js
+++ script.js
@@ -18,4 +18,40 @@ console.log(res3);
 const res4 = document.querySelectorAll("div")
 console.log(res4);
 */
+{/* <label for="email">Email</label> <br>
+ <input type="email" id="email"></input> */}
+// var label = document.createElement("label")
+// label.innerHTML="Email"
+// label.setAttribute("for","email")
+// var linebreak = document.createElement("br")
+// var input = document.createElement("input")
+// input.setAttribute("type","email");
+// input.setAttribute("id","email");
+// document.body.append(label,linebreak,input)
 
+//using function we can achieve dynamic 
+
+/*function createLabel(tagname,content,attrname,attrvalue){
+    var label = document.createElement(tagname)
+  label.innerHTML=content
+  label.setAttribute(attrname,attrvalue)
+  return label;
+}
+const label1 = createLabel("label","Email","for","email")
+document.body.append(label1);
+*/
+
+const text = document.querySelector("#text1")
+const textload =() =>{
+    setTimeout(()=>{
+   text.textContent = "Developer"
+    },0)
+    setTimeout(()=>{
+        text.textContent = "Front-End Developer"
+         },4000)
+         setTimeout(()=>{
+            text.textContent = "Back-End Developer"
+             },8000)
+}
+textload();
+setInterval(textload,12000);
