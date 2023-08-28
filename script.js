00b5325a5c5ea696d9fc5cd42345c7a2759bedf0
lazzieeCoder <rupanrrc26@gmail.com>
2023-08-28 11:41:47 +0530

    dom day 2

---
+++ script.js
@@ -0,0 +1,21 @@
+//getelementbyid
+/*const res = document.getElementById("container");
+console.log(res);
+*/
+//getelementbyclassname
+/*const res1 = document.getElementsByClassName("main");
+console.log(res1);
+*/
+
+//getelementbytagname
+/*const res2 = document.getElementsByTagName("div");
+console.log(res2);
+*/
+
+//query selector and query selector all
+/*const res3 = document.querySelector("div")
+console.log(res3);
+const res4 = document.querySelectorAll("div")
+console.log(res4);
+*/
+
